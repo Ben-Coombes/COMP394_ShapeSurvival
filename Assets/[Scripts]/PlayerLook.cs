f74d2fb2e6ac68cb33a8979be7e292f67d9ba76d
Ben Coombes <benzcoombes@gmail.com>
2022-10-11 20:46:12 -0400

    Start of the Player Character Controller

---
+++ Assets/[Scripts]/PlayerLook.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera camera;
+
+    private float xRotation = 0f;
+
+    public float xSensitivity = 30f;
+
+    public float ySensitivity = 30f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void Look(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+    }
+}
