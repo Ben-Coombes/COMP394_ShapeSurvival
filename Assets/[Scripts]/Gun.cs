6151077122a763d6fe55c52213bfc91b4d1a1e13
Ben Coombes <benzcoombes@gmail.com>
2022-10-12 22:31:56 -0400

    start of gun setup

---
+++ Assets/[Scripts]/Gun.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Unity")]
+    public GameObject bulletPrefab;
+    public Camera playerCam;
+    public Transform bulletSpawnPoint;
+    public GameObject muzzleFlash;
+
+    [Header("Bullet Stats")]
+    public float speed, upwardSpeed;
+
+    [Header("Gun Stats")]
+    public float fireRate, spread;
+    public float bulletsPerTap, fireRateTap;
+    public bool automatic;
+    
+    private int bulletsShot;
+    private bool shooting, canFire;
+
+    public bool allowInvoke = true;
+
+    public void CheckInput(InputAction action)
+    {
+        if (automatic)
+            shooting = action.IsPressed();
+        else
+            shooting = action.WasPerformedThisFrame();
+
+        if (shooting && canFire)
+        {
+            bulletsShot = 0;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        canFire = false;
+        bulletsShot++;
+
+        Ray ray = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75);
+
+        Vector3 direction = targetPoint - bulletSpawnPoint.position;
+
+        float xSpread = Random.Range(-spread, spread);
+        float ySpread = Random.Range(-spread, spread);
+
+        Vector3 directionWithSpread = direction + new Vector3(xSpread, ySpread, 0);
+
+        GameObject currentBullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * speed, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(playerCam.transform.up * upwardSpeed, ForceMode.Impulse);
+
+        if (muzzleFlash != null)
+        {
+            Instantiate(muzzleFlash, bulletSpawnPoint.position, Quaternion.identity);
+        }
+
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", fireRate);
+            allowInvoke = false;
+        }
+
+        if (bulletsShot < bulletsPerTap)
+        {
+            Invoke("Shoot", fireRateTap);
+        }
+    }
+
+    private void ResetShot()
+    {
+        canFire = true;
+        allowInvoke = true;
+    }
+}
---
+++ Assets/[Scripts]/Player/InputManager.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+
+    private PlayerInputActions playerInputActions;
+    private PlayerInputActions.OnGroundActions onGround;
+
+    private PlayerController playerController;
+
+    private PlayerLook playerLook;
+
+    private Gun playerGun;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerInputActions = new PlayerInputActions();
+        onGround = playerInputActions.OnGround;
+        playerController = GetComponent<PlayerController>();
+        playerLook = GetComponent<PlayerLook>();
+        playerGun = GetComponent<Gun>();
+        onGround.Jump.performed += ctx => playerController.Jump();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerController.Move(onGround.Movement.ReadValue<Vector2>());
+        playerGun.CheckInput(onGround.Fire);
+    }
+
+    void LateUpdate()
+    {
+        playerLook.Look(onGround.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onGround.Enable();
+    }
+    private void OnDisable()
+    {
+        onGround.Disable();
+    }
+}
