6e4e05d47cf99739bf4e9be8a57c7386d50188c4
grant <gmacmil3@my.centennialcollege.ca>
2022-10-17 18:42:09 -0400

    level XP bar text

--- Assets/[Scripts]/LevelUI.cs
+++ Assets/[Scripts]/LevelUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelUI : MonoBehaviour
 {
@@ -9,10 +10,17 @@ public class LevelUI : MonoBehaviour
     public GameObject xpBarUI;
     public Slider xpBarSlider;
 
-    
+    public TextMeshProUGUI currentLevelText;
+    public TextMeshProUGUI NextLevelText;
+
+    int nextLevel;
 
     public void UpdateXP(float xp, int level)
     {
+        nextLevel = level + 1;
         xpBarSlider.value = xp;
+
+        currentLevelText.text = "Level "+level;
+        NextLevelText.text = "Level " + nextLevel;
     }
 }
