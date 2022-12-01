c6d403e481831fd97fa3d38d5cc6bf28b1f2e571
grant <gmacmil3@my.centennialcollege.ca>
2022-12-01 00:13:52 -0500

    animator function script

---
+++ Assets/[Scripts]/Menu/AnimatorFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+	[SerializeField] GameObject obj;
+	public bool disableOnce;
+
+	void PlaySound(AudioClip whichSound){
+		if(!disableOnce){
+			obj.GetComponent<AudioSource>().PlayOneShot (whichSound);
+		}else{
+			disableOnce = false;
+		}
+	}
+}	
